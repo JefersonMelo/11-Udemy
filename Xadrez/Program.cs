8d2c4e210aef85ab5b1237bce9ce6938de3008cf
Jeferson Melo <jefsdemelo@gmail.com>
2021-03-03 14:15:35 -0300

    commit melhorando imprimirtabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -6,9 +6,11 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            //Tabuleiro tab = new Tabuleiro(8, 8);
-
-            //Tela.ImprimirTabuleiro(tab);
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.ColocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
+            tab.ColocarPeca(new Rei(tab, Cor.Branco), new Posicao(0, 1));
+            Tela.ImprimirTabuleiro(tab);
 
             PosicaoXadrez pos = new PosicaoXadrez('c', 7);
             Console.WriteLine(pos);
--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -8,6 +8,8 @@ namespace Xadrez
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write($"{8 - i} ");
+
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.Peca(i, j) == null)
@@ -16,10 +18,27 @@ namespace Xadrez
                     }
                     else
                     {
-                        Console.Write($"{tab.Peca(i, j)} ");
+                        ImprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H ");
+        }
+
+        public static void ImprimirPeca(Peca p)
+        {
+            if (p.Cor == Cor.Branco)
+            {
+                Console.Write(p);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(p);
+                Console.ForegroundColor = aux;
             }
         }
     }
